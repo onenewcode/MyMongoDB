3579026740e94079f89ee597697487f5ee8982fd
29071 <lovestudy@qq.com>
2023-10-17 17:47:54 +0800

    myMongoDB demo first

---
+++ src/main/java/onenewcode/article/ArticleApplication.java
@@ -0,0 +1,12 @@
+package onenewcode.article;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleApplication.class, args);
+    }
+}
---
+++ src/main/java/onenewcode/article/dao/CommentRepository.java
@@ -0,0 +1,13 @@
+package onenewcode.article.dao;
+
+import onenewcode.article.po.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface CommentRepository extends MongoRepository<Comment,String > {
+
+
+    Page<Comment> findByParentid(String parentid,Pageable pageable);
+
+}
